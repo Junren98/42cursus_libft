12a6f1e8d72b5015c18cf78356243eb5233c4bd1
junren98 <jun.ren@live.com>
2021-10-05 21:40:31 +0800

    upd ftput

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: tjun-ren <tjun-ren@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 14:48:58 by tjun-ren          #+#    #+#             */
-/*   Updated: 2021/09/24 02:38:10 by tjun-ren         ###   ########.fr       */
+/*   Updated: 2021/10/05 21:33:10 by tjun-ren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
+	if (!s)
+		return ;
 	ft_putstr_fd(s, fd);
 	ft_putchar_fd('\n', fd);
 }
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: tjun-ren <tjun-ren@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 14:41:59 by tjun-ren          #+#    #+#             */
-/*   Updated: 2021/09/24 02:32:03 by tjun-ren         ###   ########.fr       */
+/*   Updated: 2021/10/05 21:33:19 by tjun-ren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
+	if (!s)
+		return ;
 	write(fd, s, ft_strlen(s));
 }
